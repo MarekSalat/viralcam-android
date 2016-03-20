b17d21f6d8794e6cd960f2bc045ae87b33a22892
Marek Salat <salat.marek42@gmail.com>
2016-03-20 10:48:51 +0100

    Improve few strings

--- app/src/main/java/com/salat/viralcam/app/activities/IntroductionActivity.java
+++ app/src/main/java/com/salat/viralcam/app/activities/IntroductionActivity.java
@@ -13,8 +13,8 @@ public class IntroductionActivity extends AppIntro2 {
     @Override
     public void init(Bundle savedInstanceState) {
         addSlide(AppIntroFragment.newInstance("ViralCam", "Change the reality", R.drawable.viralcam_highres_icon_512x512, 0xFF4a148c));
-        addSlide(AppIntroFragment.newInstance("Capture the scene", "Select image you want to edit. You can also change the foreground image transparency by swiping over the image.", R.drawable.ic_camera_white_48dp, 0xFF4a148c));
-        addSlide(AppIntroFragment.newInstance("Roughly mark the edge", "It helps to better estimate what is foreground. You do not have to be precise. It can be tuned later.", R.drawable.ic_gesture_white_48dp, 0xFF4a148c));
+        addSlide(AppIntroFragment.newInstance("Capture the scene", "Select an image you want to edit. You can also change the blend by swiping over the image.", R.drawable.ic_camera_white_48dp, 0xFF4a148c));
+        addSlide(AppIntroFragment.newInstance("Roughly mark the edge", "It helps for better estimate what is foreground. You do not have to be precise. It can be tuned later.", R.drawable.ic_gesture_white_48dp, 0xFF4a148c));
         addSlide(SampleSlideFragment.newInstance(R.layout.introduction_slide_1));
         addSlide(AppIntroFragment.newInstance("Share and profit", "Show your composition to the world.", R.drawable.ic_share_white_48dp, 0xFF4a148c));
 //        addSlide(AppIntroFragment.newInstance("", "", R.drawable.placeholder_image, 0xFF4a148c));
